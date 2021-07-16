29802dddc969147567b310badaa668c53fcdb4fa
RocioA <rochyaltamirano@gmail.com>
2021-07-16 12:02:16 -0300

    Ejemplo de Before & After

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,8 +1,23 @@
 describe ('Search test cases', function(){
-    it ('Search with results', function(){
+
+    before (function(){
+        cy.log("Ejecutando precondiciones a las pruebas");
+    })
+
+    after(function(){
+        cy.log("Ejecutando todas las postcondiciones a las pruebas");
+    })
+
+    it ('Search dresses', function(){
         cy.visit('http://automationpractice.com/index.php');
         cy.get('#search_query_top').type('dress');
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains('"dress"');
-    })
+    });
+    it ('Search hats', function(){
+        cy.visit('http://automationpractice.com/index.php');
+        cy.get('#search_query_top').type('hat');
+        cy.get('#searchbox > .btn').click();
+        cy.get('.lighter').contains('"hat"');
+    });
 })
