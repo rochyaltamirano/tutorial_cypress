3ee666d1e457c0467b9363c66b9f4ecd1c610722
RocioA <rochyaltamirano@gmail.com>
2021-07-16 13:32:33 -0300

    Ejemplo de BeforeEach & AfterEach

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -8,16 +8,23 @@ describe ('Search test cases', function(){
         cy.log("Ejecutando todas las postcondiciones a las pruebas");
     })
 
-    it ('Search dresses', function(){
+    beforeEach(function(){
         cy.visit('http://automationpractice.com/index.php');
+    })
+
+    afterEach(function(){
+        cy.log ("Poner datos en su estado original");
+    })
+
+    it ('Search dresses', function(){      
         cy.get('#search_query_top').type('dress');
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains('"dress"');
-    });
+    })
+
     it ('Search hats', function(){
-        cy.visit('http://automationpractice.com/index.php');
         cy.get('#search_query_top').type('hat');
         cy.get('#searchbox > .btn').click();
         cy.get('.lighter').contains('"hat"');
-    });
+    })
 })
