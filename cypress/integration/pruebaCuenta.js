979a655c7e9972cca740e8ba4f923ee3d1bff55a
RocioA <rochyaltamirano@gmail.com>
2021-07-19 15:37:38 -0300

    Ejemplos básicos con asserts

--- cypress/integration/pruebaCuenta.js
+++ cypress/integration/pruebaCuenta.js
@@ -1,11 +1,40 @@
 describe('Cuenta', function(){
-    var a= 1+1;
-    var b= 2;
-    it ('Vamos a ver que se sume bien', function(){
-        expect(a==b).to.equal(true);
-    });
+    var a= 1;
 
-    it ('Vamos a ver que reste bien', function(){
-        expect(a-b).to.equal(0);
-    });
+    it ('Vamos a ver una igualdad', function(){
+        expect(1==1).to.equal(true);
+    })
+
+    it('Vamos a ver que no es una igualdad', function(){
+        expect(1==2).to.equal(false);
+    })
+
+    it('Vamos a ver que una resta esté bien', function(){
+        expect(1-1).to.equal(0);
+    })
+
+    it('Vamos a ver que una resta NO esté bien', function(){
+        expect(1-1).to.not.equal(2);
+    })
+
+    //Otra forma de validar
+    it('Tiene que ser verdadero', function(){
+        expect(2==2).to.be.true;
+    })
+
+    it('Tiene que ser falso', function(){
+        expect(2==1).to.be.false;
+    })
+
+    it('La variable existe', function(){
+        expect(a).to.exist;
+    })
+
+    it('Es menor a 10', function(){
+        expect(a).to.be.lessThan(10);
+    })
+
+    it('Es mayor a 10', function(){
+        expect(25).to.be.greaterThan(10);
+    })
 })
