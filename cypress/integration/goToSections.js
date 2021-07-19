7ad79a03bc844af17421c527bcb4da93490e19ce
RocioA <rochyaltamirano@gmail.com>
2021-07-19 15:18:56 -0300

    Seleccionar cualquier elemento de una lista de elementos con igual locator

--- cypress/integration/goToSections.js
+++ cypress/integration/goToSections.js
@@ -1,7 +1,14 @@
 describe ("Go to different section", function(){
-    it("Go to Women section", function(){
+    beforeEach(function(){
         cy.visit('http://automationpractice.com/index.php');
+    })
+    //Selecciona el primer elemento
+    it("Go to Women section", function(){
         cy.get('[class=sf-with-ul]').first().click();
     })
 
+    //Selecciona el elemento pasado por parámetro en 'eq'
+    it ("Go to Dress section", function(){
+        cy.get('[class=sf-with-ul]').eq(3).click();
+    })
 })
