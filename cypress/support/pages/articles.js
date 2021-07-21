693241442c60f1c4488a1d37a20f1d4d0fe6b438
RocioA <rochyaltamirano@gmail.com>
2021-07-21 16:05:39 -0300

    Ejemplo de uso de fixtures

--- cypress/support/pages/articles.js
+++ cypress/support/pages/articles.js
@@ -1,10 +1,9 @@
 class articlesPage{
-    constructor(){
-        this.banner = '.lighter';
-    }
 
     verifyArticle = (article) => {
-        cy.get(this.banner).contains(article);
+        cy.fixture('articles.json').then((locators) => {
+            cy.get(locators.banner).contains(article);
+        })
     }
 }
 
--- cypress/support/pages/index.js
+++ cypress/support/pages/index.js
@@ -1,13 +1,9 @@
 class indexPage{
-    constructor(){
-        this.searchInput = '#search_query_top';
-        this.searchButton = '#searchbox > .btn';
+    search = (element) => { 
+        cy.fixture('index.json').then((locators)=>{
+            cy.get(locators.searchInput).type(element);
+            cy.get(locators.searchButton).click();
+        })
     }
-
-    search = (element) =>{
-        cy.get(this.searchInput).type(element);
-        cy.get(this.searchButton).click();
-    }
-
 }
 export default new indexPage();
